894d40d9305b3cc46f7938dd8301da4f58cb5696
dhimaseka <dimaseka83@gmail.com>
2022-07-20 16:50:53 +0700

    ++

--- resources/js/emiindo/Index.vue
+++ resources/js/emiindo/Index.vue
@@ -69,7 +69,10 @@
                             <div class="col-12">
                                 <div class="card card-detail removeshadow">
                                     <div class="card-body border-0">
-                                        <h5 class="card-title pl-2 py-2"><b>{{ detailModalSO.instansi.instansinm }}</b></h5>
+                                        <h5 class="card-title pl-2 py-2">
+                                            <b v-if="detailModalSO.instansi == null">-</b>
+                                            <b v-else>{{ detailModalSO.instansi.instansinm }}</b>
+                                            </h5>
                                         <ul class="fa-ul card-text">
                                             <li class="py-2"><span class="fa-li"><i
                                                         class="fas fa-user-alt fa-fw"></i></span>
@@ -160,6 +163,8 @@
                                                                         <th rowspan="2">Produk</th>
                                                                         <th colspan="2">Qty</th>
                                                                         <th rowspan="2">Harga</th>
+                                                                        <th rowspan="2">Ongkos Kirim</th>
+                                                                        <th rowspan="2">Subtotal</th>
                                                                     </tr>
                                                                     <tr>
                                                                         <th><i class="fas fa-shopping-cart"></i>
@@ -179,12 +184,14 @@
                                                                             {{ item.qty }} {{ item.datauom.uom }}
                                                                         </td>
                                                                         <td rowspan="1" class="nowraptxt tabnum">Rp. {{ formatRupiah(parseInt(item.price)) }}</td>
+                                                                        <td rowspan="1" class="nowraptxt tabnum">Rp. {{ formatRupiah(parseInt(item.shippingcharge)) }}</td>
+                                                                        <td rowspan="1" class="nowraptxt tabnum">Rp. {{ formatRupiah(subtotal(item.qty,item.price,item.shippingcharge)) }}</td>
                                                                     </tr>
 
                                                                 </tbody>
                                                                 <tfoot>
                                                                     <tr>
-                                                                        <td colspan="4">Total Harga
+                                                                        <td colspan="6">Total Harga
                                                                         </td>
                                                                         <td class="tabnum nowraptxt">Rp. {{ formatRupiah(total) }}
                                                                         </td>
@@ -201,6 +208,10 @@
                             </div>
                         </div>
                     </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" @click="checklist(detailModalSO.epurno)">Checklist</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -216,7 +227,7 @@ import axios from 'axios'
                 detailModalSO: [],
                 modal: false,
                 checkAllStatus: false,
-                tab: 'ekatalog',
+                tab: 'salesorder',
                 checkEkat: [],
                 checkSpa: [],
             }
@@ -272,15 +283,34 @@ import axios from 'axios'
                 const format = e.toString().split('').reverse().join('');
                 const convert = format.match(/\d{1,3}/g);
                 return convert.join('.').split('').reverse().join('');
-            }
+            },
 
+            subtotal(qty, price, ongkir) {
+                return (parseInt(qty) * parseInt(price)) + parseInt(ongkir);
+            },
+            checklist(id) {
+                this.dataSO.filter(item => {
+                    if (item.epurno == id) {
+                        this.checkEkat.push(item);
+                    }
+                });
+                this.$refs.cbcheck.forEach(item => {
+                    if (item.value == id) {
+                        item.checked = true;
+                    }
+                });
+            },
         },
         mounted() {
             this.loadData();
         },
         computed: {
             total() {
-                return this.detailModalSO.sodetail.reduce((a, b) => a + parseInt(b.price), 0);
+                let total = 0;
+                this.detailModalSO.sodetail.forEach(item => {
+                    total += this.subtotal(item.qty, item.price, item.shippingcharge);
+                });
+                return total;
             }
         },
         updated() {
@@ -289,3 +319,210 @@ import axios from 'axios'
     }
 
 </script>
+    <style scoped>
+
+ul#status {
+    padding: 0;
+}
+        ul#status li {
+            /* float: left; */
+            display:inline;
+            padding: 0;
+            list-style-type: none;
+            margin: 0; /* To remove default bottom margin */
+            /* margin: 10px; */
+        }
+
+        .alert-danger {
+            color: #a94442;
+            background-color: #f2dede;
+            border-color: #ebccd1;
+        }
+
+        .separator {
+            border-top: 1px solid #bbb;
+            width: 90%;
+        }
+
+        .wb {
+            word-break: break-all;
+            white-space: normal;
+        }
+
+        .nowraptxt {
+            white-space: nowrap;
+        }
+
+        .filter {
+            margin: 5px;
+        }
+
+        /* thead {
+            text-align: center;
+        }
+
+        td {
+            text-align: center;
+            white-space: nowrap;
+        } */
+
+        #urgent {
+            color: #dc3545;
+            font-weight: 600;
+        }
+
+        #warning {
+            color: #FFC700;
+            font-weight: 600;
+        }
+
+        #info {
+            color: #3a7bb0;
+            font-weight: 600;
+        }
+
+        .minimizechar {
+            display: inline-block;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            max-width: 13ch;
+        }
+
+        .hide {
+            display: none;
+        }
+
+        .dropdown-toggle:hover {
+            color: #4682B4;
+        }
+
+        .dropdown-toggle:active {
+            color: #C0C0C0;
+        }
+
+        td.details-control {
+            content: "\f055";
+            font-family: FontAwesome;
+            left: -5px;
+            position: absolute;
+            top: 0;
+        }
+
+        #detailekat {
+            background-color: #E9DDE5;
+
+        }
+
+        #detailspa {
+            background-color: #FFE6C9;
+        }
+
+        #detailspb {
+            background-color: #E1EBF2;
+            /* color: #7D6378; */
+
+        }
+
+        .tabnum {
+            font-variant-numeric: tabular-nums;
+        }
+
+        .removeshadow {
+            box-shadow: none;
+        }
+
+        .align-center {
+            text-align: center;
+        }
+
+        .bordertopnone {
+            border-top: 0;
+            border-left: 0;
+            border-right: 0;
+            border-bottom: 0;
+            vertical-align: top;
+        }
+
+        .margin {
+            margin-left: 10px;
+            margin-right: 10px;
+            margin-top: 15px;
+            margin-bottom: 15px;
+        }
+
+        .card-detail {
+            align-items: center;
+            flex-direction: row;
+            shadow: none;
+            border: none;
+        }
+
+        .card-detail img {
+            width: 25%;
+            border-top-right-radius: 0;
+            border-bottom-left-radius: calc(0.25rem - 1px);
+        }
+
+        @media screen and (min-width: 1440px) {
+
+            body {
+                font-size: 14px;
+            }
+
+            #detailmodal {
+                font-size: 14px;
+            }
+
+            .btn {
+                font-size: 14px;
+            }
+
+            .overflowy {
+                max-height: 550px;
+                width: auto;
+                overflow-y: scroll;
+                box-shadow: none;
+            }
+
+            .labelket {
+                text-align: right;
+            }
+        }
+
+        @media screen and (max-width: 1439px) {
+            body {
+                font-size: 12px;
+            }
+
+            h4 {
+                font-size: 20px;
+            }
+
+            #detailmodal {
+                font-size: 12px;
+            }
+
+            .btn {
+                font-size: 12px;
+            }
+
+            .overflowy {
+                max-height: 450px;
+                width: auto;
+                overflow-y: scroll;
+                box-shadow: none;
+            }
+
+            .labelket {
+                text-align: right;
+            }
+        }
+
+        @media screen and (max-width: 991px) {
+            .labelket {
+                text-align: left;
+            }
+        }
+
+    </style>
