d5f84925343b9e44be5c89e21b51a1fc3409ea6c
dhimaseka <dimaseka83@gmail.com>
2022-11-30 13:44:21 +0700

    ++

--- resources/js/teknik/page/po/detail/header.vue
+++ resources/js/teknik/page/po/detail/header.vue
@@ -14,5 +14,28 @@ export default {
     </div>
 </template>
 <style>
-    
+    .card-primary {
+        background-color: #eff2f4;
+        color: #5b7a8f;
+    }
+
+    .card-danger {
+        background-color: #f5efef;
+        color: #985d61;
+    }
+
+    .card-success {
+        background-color: #e7e9e7;
+        color: #616e61;
+    }
+
+    .card-secondary {
+        background-color: #d8d8d8;
+        color: #616161;
+    }
+
+    .row-whith-wide-cards .card {
+        width: 100%;
+    }
+
 </style>
