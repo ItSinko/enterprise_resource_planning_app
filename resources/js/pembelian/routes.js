2818e18e7afa7021b040989935c0f8939ea65f74
dhimaseka <dimaseka83@gmail.com>
2022-12-26 13:02:00 +0700

    ++

--- resources/js/pembelian/routes.js
+++ resources/js/pembelian/routes.js
@@ -51,7 +51,7 @@ const routes = [
         name: "permintaan",
     },
     {
-        path: "/pembelian/create",
+        path: "/pembelian/permintaan/dalamproses/create",
         component: PermintaanTambahDalamProses,
         name: "permintaanTambahDalamProses",
     },
