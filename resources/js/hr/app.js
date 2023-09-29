6ee607faee6cc2ded7b44b3e4d9dd39a5b72a013
dhimaseka <dimaseka83@gmail.com>
2023-09-29 14:22:41 +0700

    ++

--- resources/js/hr/app.js
+++ resources/js/hr/app.js
@@ -2,16 +2,17 @@ import Vue from 'vue'
 import Index from './Index.vue'
 import router from './routes'
 import VueRouter from 'vue-router';
-import Vuetify from 'vuetify'
 import VueSweetalert2 from "vue-sweetalert2"
 import Vuex from 'vuex';
 import storeData from './store'
 import DateFormat from './plugins/dateFormat'
 import "vue-select/dist/vue-select.css";
 
+import 'jquery/dist/jquery.js'
+import 'bootstrap/dist/js/bootstrap.js'
+
 window.Vue = require('vue').default;
 Vue.use(VueRouter);
-Vue.use(Vuetify)
 Vue.use(VueSweetalert2);
 Vue.use(DateFormat);
 
@@ -21,7 +22,6 @@ const app = new Vue({
     el: '#app',
     store,
     router,
-    vuetify: new Vuetify(),
     components: {
         "index": Index
     }
