691186d83025d1f0e2f01fba7a94fd8a644d8239
dhimaseka <dimaseka83@gmail.com>
2023-09-29 15:38:20 +0700

    ++

--- resources/js/hr/components/uploadFile.vue
+++ resources/js/hr/components/uploadFile.vue
@@ -117,16 +117,20 @@ export default {
             <button type="button" class="clearButton" @click="reset">
                 {{ clearAll ? clearAll : "clear All" }}
             </button>
-            <div class="imageHolder" v-for="(img, i) in Imgs" :key="i">
-                <span>{{ files[i].name }}</span>
-                <span class="delete" style="color: white" @click="deleteImg(--i)">
-                    <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                    </svg>
-                </span>
-                <div class="plus" @click="append" v-if="++i == Imgs.length">+</div>
+            <div class="row">
+                <div class="col-3" v-for="(img, i) in Imgs" :key="i">
+                    <div class="imageHolder">
+                        <span>{{ files[i].name }}</span>
+                        <span class="delete" style="color: white" @click="deleteImg(--i)">
+                            <svg class="icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </span>
+                        <div class="plus" @click="append" v-if="++i == Imgs.length">+</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
