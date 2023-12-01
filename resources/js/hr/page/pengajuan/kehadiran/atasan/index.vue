7ccd972be32c6a4cf68ba1e84b155a66095454fb
your_username <dimaseka83@gmail.com>
2023-12-01 12:47:32 +0700

    ++

--- resources/js/hr/page/pengajuan/kehadiran/atasan/index.vue
+++ resources/js/hr/page/pengajuan/kehadiran/atasan/index.vue
@@ -1,10 +1,55 @@
 <script>
+import pengajuan from './pengajuan.vue';
+import riwayat from './riwayat.vue';
 export default {
-    
+    components: {
+        pengajuan,
+        riwayat
+    },
+    data() {
+        return {
+                        dataPengajuan: [
+                {
+                    id: 1,
+                    nama: 'Prita Hanifah',
+                    bagian: 'General Affair',
+                    tanggal: '03 Januari 2021',
+                    tgl_pengajuan: '2021-01-03',
+                    jenis: 'Izin Keperluan Kantor',
+                },
+                {
+                    id: 2,
+                    nama: 'Prita Hanifah',
+                    bagian: 'Produksi',
+                    tanggal: '03 Januari 2021',
+                    tgl_pengajuan: '2021-01-03',
+                    jenis: 'Izin Keperluan Kantor',
+                },
+            ],
+        }
+    },
 }
 </script>
 <template>
-    <div>
-        
+    <div class="card">
+        <div class="card-body">
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="pills-pengajuanAtasan-tab" data-toggle="pill" data-target="#pills-pengajuanAtasan"
+                        type="button" role="tab" aria-controls="pills-pengajuanAtasan" aria-selected="true">Pengajuan</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-riwayatAtasan-tab" data-toggle="pill" data-target="#pills-riwayatAtasan"
+                        type="button" role="tab" aria-controls="pills-riwayatAtasan" aria-selected="false">Riwayat</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-pengajuanAtasan" role="tabpanel" aria-labelledby="pills-pengajuanAtasan-tab">
+                    <pengajuan :dataTable="dataPengajuan" />
+                </div>
+                <div class="tab-pane fade" id="pills-riwayatAtasan" role="tabpanel" aria-labelledby="pills-riwayatAtasan-tab">
+                    <riwayat />
+                </div>
+            </div>
     </div>
-</template>
+</div></template>
---
+++ resources/js/hr/page/pengajuan/kehadiran/atasan/pengajuan.vue
@@ -0,0 +1,143 @@
+<script>
+import DataTable from '../../../../components/DataTable.vue'
+export default {
+    components: {
+        DataTable,
+    },
+    props: ['dataTable'],
+    data() {
+        return {
+            headers: [
+                { text: 'No', value: 'no' },
+                { text: 'Nama', value: 'nama' },
+                { text: 'Bagian', value: 'bagian', sortable: false },
+                { text: 'Tanggal Pengajuan', value: 'tanggal', sortable: false },
+                { text: 'Jenis Pelaporan', value: 'jenis' },
+                { text: 'Aksi', value: 'aksi' },
+            ],
+            search: '',
+            tanggalAwal: '',
+            tanggalAkhir: '',
+            bagianFilter: [],
+        }
+    },
+    methods: {
+        setuju(item) {
+            this.$swal({
+                title: 'Apakah anda yakin, ingin menyetujui pengajuan ini?',
+                text: "Pengajuan akan masuk ke dalam daftar kehadiran anggota",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#28a745',
+                cancelButtonColor: '#dc3545',
+                confirmButtonText: 'Ya, Setujui!',
+                cancelButtonText: 'Batal',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.$swal({
+                        title: 'Berhasil!',
+                        text: 'Pengajuan berhasil disetujui',
+                        icon: 'success',
+                        confirmButtonColor: '#28a745',
+                    })
+                }
+            })
+        },
+        tolak(item) {
+            this.$swal({
+                title: 'Apakah anda yakin, ingin menolak pengajuan ini?',
+                text: "Pengajuan akan masuk ke dalam daftar pengajuan batal",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#28a745',
+                cancelButtonColor: '#dc3545',
+                confirmButtonText: 'Ya, Tolak!',
+                cancelButtonText: 'Batal',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.$swal({
+                        title: 'Berhasil!',
+                        text: 'Pengajuan berhasil ditolak',
+                        icon: 'success',
+                        confirmButtonColor: '#28a745',
+                    })
+                }
+            })
+        },
+        detail(item) {
+            this.$router.push({
+                name: 'detail-pengajuan',
+                params: {
+                    id: item.id,
+                    link: this.$route.fullPath,
+                }
+            })
+        },
+        renderNo(data) {
+            return data.map((item, index) => {
+                return {
+                    ...item,
+                    no: index + 1
+                }
+            })
+        }
+    },
+    computed: {
+        filterData() {
+            let filtered = this.renderNo(this.dataTable);
+
+            if (this.tanggalAwal && this.tanggalAkhir) {
+                const startDate = new Date(this.tanggalAwal);
+                startDate.setHours(0, 0, 0, 0);
+
+                const endDate = new Date(this.tanggalAkhir);
+                endDate.setHours(23, 59, 59, 999);
+
+                filtered = filtered.filter(item => {
+                    const date = new Date(item.tgl_pengajuan);
+                    return date >= startDate && date <= endDate;
+                })
+            } else if (this.tanggalAwal) {
+                const startDate = new Date(this.tanggalAwal);
+                startDate.setHours(0, 0, 0, 0);
+
+                filtered = filtered.filter(item => {
+                    const date = new Date(item.tgl_pengajuan);
+                    return date >= startDate;
+                })
+            } else if (this.tanggalAkhir) {
+                const endDate = new Date(this.tanggalAkhir);
+                endDate.setHours(23, 59, 59, 999);
+
+                filtered = filtered.filter(item => {
+                    const date = new Date(item.tgl_pengajuan);
+                    return date <= endDate;
+                })
+            }
+
+            if (this.bagianFilter.length > 0) {
+                filtered = filtered.filter(item => {
+                    return this.bagianFilter.includes(item.bagian);
+                })
+            }
+
+            return filtered;
+        },
+
+        getAllBagianUnique() {
+            const bagian = this.dataTable.map(item => item.bagian);
+            return [...new Set(bagian)];
+        }
+    }
+}
+</script>
+<template>
+    <div>
+        <div class="d-flex flex-row-reverse bd-highlight">
+            <div class="p-2 bd-highlight">
+                <input type="text" v-model="search" class="form-control">
+            </div>
+        </div>
+        <DataTable :headers="headers" :items="filterData" :search="search" />
+    </div>
+</template>
